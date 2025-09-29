e8b9eec0987c415ae6bf792de4af2659499a78b1
Gabriel-Menezes-Morais <gabriel.menezes.mata@gmail.com>
2025-09-29 19:29:24 -0300

    Realização do TAD server_addr

--- servidor.c
+++ servidor.c
@@ -9,3 +9,25 @@
 #define MAX_CLIENTS 10 // Número máximo de clientes (para o futuro)
 #define BUFFER_SIZE 2048 // Tamanho do buffer para as mensagens
 
+int main(int argc, char *argv[]){
+
+    if(argc != 2){
+        printf("Modo de uso: %s <numero_da_porta>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int porta = atoi(argv[1]);
+
+    int server_socket;
+    int client_socket;  
+
+    struct sockaddr_in server_addr; 
+    struct sockaddr_in client_addr;
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(porta);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+}
